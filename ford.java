454fdbc56fde6b7e45a091fcf8d9a232e6be0b76
Sharanya M S <sharanyamavinaguni@gmail.com>
2024-08-02 10:27:03 +0530

    Add files via upload

---
+++ ford.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class ford {
+    static class Edge {
+        int src, dest, weight;
+        Edge(int src, int dest, int weight) {
+            this.src = src;
+            this.dest = dest;
+            this.weight = weight;
+        }
+    }
+
+    public static void bellmanFord(int V, List<Edge> edges, int src) {
+        int[] dist = new int[V];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        for (int i = 1; i < V; i++) {
+            for (Edge edge : edges) {
+                if (dist[edge.src] != Integer.MAX_VALUE && dist[edge.src] + edge.weight < dist[edge.dest]) {
+                    dist[edge.dest] = dist[edge.src] + edge.weight;
+                }
+            }
+        }
+
+        for (Edge edge : edges) {
+            if (dist[edge.src] != Integer.MAX_VALUE && dist[edge.src] + edge.weight < dist[edge.dest]) {
+                System.out.println("Graph contains negative weight cycle");
+                return;
+            }
+        }
+
+        System.out.println("Vertex Distance from Source");
+        for (int i = 0; i < V; i++)
+            System.out.println(i + "\t\t" + dist[i]);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of vertices and edges:");
+        int V = sc.nextInt();
+        int E = sc.nextInt();
+
+        List<Edge> edges = new ArrayList<>();
+        System.out.println("Enter the edges with source, destination, and weight:");
+        for (int i = 0; i < E; i++) {
+            int src = sc.nextInt();
+            int dest = sc.nextInt();
+            int weight = sc.nextInt();
+            edges.add(new Edge(src, dest, weight));
+        }
+
+        System.out.println("Enter the source vertex:");
+        int src = sc.nextInt();
+
+        bellmanFord(V, edges, src);
+        sc.close();
+    }
+}
